13b129b87a308e3f35a33fd6b867f6ecb347f9e9
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-05-20 21:56:40 +0700

    add

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in a practical way
         </a>
       </header>
     </div>

269ef16eaab719f1f44d3a7aa9c7374e6d49e6cf
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-05-28 22:21:15 +0700

    name

--- src/App.js
+++ src/App.js
@@ -8,28 +8,23 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World This is my first_DevOps App
+          Name: Peeratchai Kleebbua
         </p>
         <p>
-          This is just a test to confirm that CI/CD is successful
-        </p>
-        <p>
-          the application for DevOps CI/CD integration and deployment on AWS s3
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in a practical way
-        </a>
-        <p>
-          <Button variant="contained"><code>src/App.js</code></Button>
-          &nbsp;&nbsp;<Button 
+          <Button 
+            variant="contained"
+            href="https://www.linkedin.com/in/peeratchai-kleebbua-380131198/"
+            target='blank'
+            rel="noopener noreferrer"
+          >
+            <code>Linkedin</code>
+          </Button>
+          &nbsp;&nbsp;
+          <Button 
             variant="contained" 
             href='https://github.com/pzalove24' 
             target='blank'
+            rel="noopener noreferrer"
             >
             Github
           </Button>
